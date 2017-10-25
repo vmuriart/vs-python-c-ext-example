2058282bb188c21639e2096d624fc58c480e83f2
Victor Uriarte <victor.m.uriarte@intel.com>
2017-10-24 17:36:50 -0700

    Add example setup.py for module
    Alternatively, can be used by adding to PATH the path of the build output

---
+++ example1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension, DEBUG
+
+sfc_module = Extension('ExampleCLibrary', sources=['module.cpp'])
+
+setup(
+    name='ExampleCLibrary',
+    version='1.0',
+    description='Python Package with superfastcode C++ Extension',
+    ext_modules=[sfc_module]
+)
